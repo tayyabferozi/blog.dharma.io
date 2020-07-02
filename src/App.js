6cc3770428656a55c7a801adbc6276d998b29e28
= <=>
2020-07-02 00:19:22 -0700

    add sidedrawer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './App.css';
+import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import Blog from "./components/Blog/Blog";
-import BlogBoxes from "./components/BlogBoxes/BlogBoxes";
+// import Blog from "./components/Blog/Blog";
+// import BlogBoxes from "./components/BlogBoxes/BlogBoxes";
+import Contact from "./components/Contact/Contact";
+// import SideDrawer from "./components/UI/SideDrawer/SideDrawer";
 import Footer from "./components/Footer/Footer";
-import Subscribe from "./components/Subscribe/Subscribe";
+// import Subscribe from "./components/Subscribe/Subscribe";
 
 class App extends Component {
-
   render() {
     return (
-      <div className="TextCenter">
-        <Navbar />
-        <Blog />
-        <BlogBoxes />
-        <Subscribe />
-        <Footer />
-      </div >
+      <React.Fragment>
+        {/* <SideDrawer /> */}
+        <div className="TextCenter">
+          <Navbar />
+          <Contact />
+          <Footer />
+        </div>
+      </React.Fragment>
     );
   }
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,20 +1,17 @@
-import React, { Fragment } from "react";
+import React from "react";
 
 import "./Footer.css";
 import Button from "../UI/Button/Button";
 
 const Footer = () => {
   return (
-    <Fragment>
-      <div className="Next">Next Page »</div>
-      <div className="Footer">
-        <Button>Twitter</Button>
-        <Button>Careers</Button>
-        <Button>Blog</Button>
-        <Button>Discord</Button>
-        <Button>Terms</Button>
-      </div>
-    </Fragment>
+    <div className="Footer">
+      <Button>Twitter</Button>
+      <Button>Careers</Button>
+      <Button>Blog</Button>
+      <Button>Discord</Button>
+      <Button>Terms</Button>
+    </div>
   );
 };
 
